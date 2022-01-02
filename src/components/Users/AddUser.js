69e86b8e085a4d32130397f11b90bf4fc5305685
unknown <aalshanbayev@gmail.com>
2022-01-01 23:07:08 -0500

    app is built

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -40,9 +40,14 @@ const AddUser = props => {
     setEnteredAge(e.target.value);
   }
   
+  const errorHandler = () => {
+    setError(null);
+  }
+
+
   return (
     <>
-      {error && <ErrorModal title={error.title} message={error.message}/>}
+      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
